f990d94c5b7ceedd79e7cd78981d1e1bb776fd24
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2023-07-05 01:05:32 -0700

    Añadido NPM Boostrap Y SASS

--- index.php
+++ index.php
@@ -1,33 +1,42 @@
-<?php 
-    $error = $_GET['log']?? null;
-    $errorLogin = $_GET['err']?? null;
+<?php
+$error = $_GET['log'] ?? null;
+$errorLogin = $_GET['err'] ?? null;
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/app.css">
     <title>Dashboard</title>
 </head>
+
 <body>
     <h1>Dashboard POS-JBAN</h1>
-    <?php 
-        if(isset($errorLogin)){
-            echo "<p>ACCESO DENEGADO</p>";
-        }
-        if(isset($error)){
-            echo "<p>Credenciales Incorrectas</p>";
-        }
+    <?php
+    if (isset($errorLogin)) {
+        echo "<p>ACCESO DENEGADO</p>";
+    }
+    if (isset($error)) {
+        echo "<p>Credenciales Incorrectas</p>";
+    }
     ?>
-    <div>
-        <form action="validar.php" method="post">
-            <input name = 'usuario' type="text" placeholder="usuario" require>
-            <input name= 'pass' type="password" placeholder="contraseña" requiere> 
-            <input type="submit" value="Entrar" >
-        </form>
-        
-    </div>
+    <form action="validar.php" method="post">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" name="usuario">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" name="pass">
+        </div>
+        <button type="submit" class="btn btn-primary">Entrar</button>
+    </form>
+
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- validar.php
+++ validar.php
@@ -1,7 +1,7 @@
 <?php 
 
-    $con = new mysqli('localhost','id20924423_jban77', 'Jorgeesteban$santi1', 'id20924423_pos1');
-    //$con = new mysqli('localhost',  'jban', '', 'pos') or die("Error Mysql Conexion");
+    //$con = new mysqli('localhost','id20924423_jban77', 'Jorgeesteban$santi1', 'id20924423_pos1');
+    $con = new mysqli('localhost',  'jban', '', 'pos') or die("Error Mysql Conexion");
     
     try {
         $query = "SELECT * FROM usuarios WHERE rfc = '{$_POST['usuario']}' AND pass = '{$_POST['pass']}'"; 
